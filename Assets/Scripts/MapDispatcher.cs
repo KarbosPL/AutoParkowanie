b9991f4baa7a582f06ef09140bf02f9fb433676f
KarbosPL <karbospl@gmail.com>
2026-06-15 21:45:01 +0200

    15.06
    dalsze prace

--- Assets/Scripts/MapDispatcher.cs
+++ Assets/Scripts/MapDispatcher.cs
@@ -9,14 +9,16 @@ public class MapDispatcher : MonoBehaviour
         ScenaMapa1 m1 = GetComponent<ScenaMapa1>();
         ScenaMapa2 m2 = GetComponent<ScenaMapa2>();
         ScenaMapa3 m3 = GetComponent<ScenaMapa3>();
+        
         if (m1) m1.enabled = false;
         if (m2) m2.enabled = false;
         if (m3) m3.enabled = false;
+        
         switch (map)
         {
             case 1: if (m1) m1.enabled = true; break;
             case 2: if (m2) m2.enabled = true; break;
-            case 3: if (m2) m3.enabled = true; break;
+            case 3: if (m3) m3.enabled = true; break;
         }
     }
 }
